53dad5ca45ad3a064cc57dbccee2c051e9381a3d
WRup <rupek987>
2021-11-07 21:42:52 +0100

    Add node modules step

--- run.bat
+++ run.bat
@@ -14,6 +14,16 @@ set /p version=Prosze podac wersje bazy danych:
 if %drive%==mariadb set DATABASE_URL=mysql://%username%:%password%@%host%:%port%/labinventory?serverVersion=%drive%-%version%
 if %drive%==mysql set DATABASE_URL=%drive%://%username%:%password%@%host%:%port%/labinventory?serverVersion=%version%
 echo Following DATABASE_URL env will be created %DATABASE_URL%
+::Install node modules
+npm install
+
+::Install required libs
+call composer install
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+::Install css/js
+call yarn dev
+if %errorlevel% neq 0 exit /b %errorlevel%
 
 ::Create and fulfill database
 php bin/console doctrine:database:create
@@ -22,14 +32,6 @@ php bin/console make:migration
 if %errorlevel% neq 0 exit /b %errorlevel%
 php bin/console doctrine:migrations:migrate
 php bin/console doctrine:fixtures:load
-if %errorlevel% neq 0 exit /b %errorlevel%
-
-::Install required libs
-call composer install
-if %errorlevel% neq 0 exit /b %errorlevel%
-
-::Install css/js
-call yarn dev
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 ::start application
